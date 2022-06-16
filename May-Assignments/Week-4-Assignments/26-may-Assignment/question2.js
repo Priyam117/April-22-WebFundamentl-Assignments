bf2698140219af6b78420a0e3269982a00848247
Your Name <test@gmail.com>
2022-06-16 15:41:07 +0530

    Assignments

---
+++ May-Assignments/Week-4-Assignments/26-may-Assignment/question2.js
@@ -0,0 +1,7 @@
+let num1 = 10;
+let num2 = 20;
+if (num1 < num2) {
+    console.log("num2 is greater ");
+} else {
+    console.log("num1 is greater");
+}
---
+++ May-Assignments/Week-4-Assignments/26-may-Assignment/queston1.js
@@ -0,0 +1,13 @@
+//   
+
+let age = 15;
+if (age >= 18) {
+    console.log("eligible for voting ");
+
+} else(age < 18)
+
+
+
+{
+    console.log("not eligible for voting");
+}
