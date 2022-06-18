609cd47b7e13045f4e68b8ec1229141b024c944e
Your Name <test@gmail.com>
2022-06-18 14:57:40 +0530

    commit

---
+++ May-Assignments/Week-4-Assignments/26-may-Assignment/question3.js
@@ -0,0 +1,13 @@
+let num = -5;
+if (num === 0)
+
+{
+    console.log(num + " num is  equal to zero")
+} else
+if (num > 0)
+
+{
+    console.log(num + " number is positive ");
+} else {
+    console.log(num + " number is negative ");
+}
