ba15d4863eeb568f1d76d9ede1532a08b29bd9a0
Your Name <test@gmail.com>
2022-06-19 14:06:35 +0530

    Assignment

---
+++ May-Assignments/Week-4-Assignments/28-May-Assignment/loops.js
@@ -0,0 +1,38 @@
+// check the given number is even or not 
+
+for (let j = 1; j <= 10; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+}
+
+
+//  print all odd number between 1 to 20
+
+
+for (let n = 1; n <= 20; n += 2) {
+    console.log(n);
+}
+
+
+
+//  while loop
+
+let m = 1;
+while (m <= 5) {
+    console.log(m);
+    m += 2;
+
+}
+
+
+
+// do-while  loop
+
+
+let a = 16;
+do {
+    console.log(a);
+    a++;
+}
+while (a <= 10);
