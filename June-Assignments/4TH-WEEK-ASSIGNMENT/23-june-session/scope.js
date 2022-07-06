3269603e66d9a6ad0406a1b0972ee086cd63fcf7
Your Name <test@gmail.com>
2022-07-06 13:34:44 +0530

    Assignment submission

---
+++ June-Assignments/4TH-WEEK-ASSIGNMENT/23-june-session/scope.js
@@ -0,0 +1,17 @@
+function fn() {
+    var a = 10; // var supports function sccope 
+    if (a > 5) {
+        let b = 6; // let and const supports block scope
+        console.log(a + b);
+        console.log(a);
+    }
+    console.log(a);
+
+    function fn1() {
+        let c = 23; // it can be access only within block scope
+        console.log(a + c);
+        console.log(c);
+    }
+    fn1();
+}
+fn();
